425d14a17435985108178372a37474b06c7e3cf6
leon <cyliang1113@126.com>
2020-05-24 20:22:47 +0800

    111

---
+++ src/main/java/com/leolab/nettydemo/Demo01.java
@@ -0,0 +1,48 @@
+package com.leolab.nettydemo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workerGroup);
+
+            serverBootstrap.option(ChannelOption.SO_BACKLOG, 128);
+            serverBootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            serverBootstrap.channel(NioServerSocketChannel.class);
+
+            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LineBasedFrameDecoder(100));
+                }
+            });
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = serverBootstrap.bind(8080).sync(); // (7)
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
