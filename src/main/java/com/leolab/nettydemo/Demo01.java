43d902e36fe26cba9d4e4ce8d253ee1abf80d782
leo <cyliang1113@126.com>
2020-05-28 13:46:22 +0800

    1111

--- src/main/java/com/leolab/nettydemo/Demo01.java
+++ src/main/java/com/leolab/nettydemo/Demo01.java
@@ -1,14 +1,12 @@
 package com.leolab.nettydemo;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 public class Demo01 {
     public static void main(String[] args) {
@@ -27,16 +25,16 @@ public class Demo01 {
             serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 public void initChannel(SocketChannel ch) throws Exception {
-                    ch.pipeline().addLast(new LineBasedFrameDecoder(100));
+                    ch.pipeline().addLast(
+                            new LineBasedFrameDecoder(500),
+                            new StringDecoder(),
+                            new MyPrinter()
+                    );
                 }
             });
 
-            // Bind and start to accept incoming connections.
-            ChannelFuture f = serverBootstrap.bind(8080).sync(); // (7)
+            ChannelFuture f = serverBootstrap.bind(8080).sync();
 
-            // Wait until the server socket is closed.
-            // In this example, this does not happen, but you can do that to gracefully
-            // shut down your server.
             f.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -45,4 +43,13 @@ public class Demo01 {
             bossGroup.shutdownGracefully();
         }
     }
+
+    private static class MyPrinter extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            System.out.println("MyPrinter: " + msg);
+            ctx.fireChannelRead(msg);
+        }
+    }
+
 }
